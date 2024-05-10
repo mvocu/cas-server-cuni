1d0d2432ccf2d95f466144085f9b6b788204fe2b
Michal Voců <michal.vocu@gmail.com>
2024-05-10 11:27:26 +0200

    work

--- src/main/java/cz/cuni/cas/mfa/gauth/flow/CuniGAuthSendRequestNotificationAction.java
+++ src/main/java/cz/cuni/cas/mfa/gauth/flow/CuniGAuthSendRequestNotificationAction.java
@@ -1,22 +1,27 @@
 package cz.cuni.cas.mfa.gauth.flow;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.web.flow.actions.BaseCasWebflowAction;
+import org.apereo.cas.web.support.WebUtils;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 import org.springframework.webflow.action.EventFactorySupport;
 
 @Slf4j
+@RequiredArgsConstructor
 public class CuniGAuthSendRequestNotificationAction extends BaseCasWebflowAction {
 
-    public CuniGAuthSendRequestNotificationAction(CasConfigurationProperties casProperties) {
-        super();
-    }
+    private final CasConfigurationProperties casProperties;
 
     @Override
     protected Event doExecute(RequestContext requestContext) throws Exception {
-        LOGGER.debug("XXX Executing action CuniGAuthSendRequestNotificationAction");
+        val authentication = WebUtils.getAuthentication(requestContext);
+        val principal = authentication.getPrincipal();
+
+        LOGGER.debug("XXX Executing action CuniGAuthSendRequestNotificationAction for principal [{}]", principal.getId());
         return new EventFactorySupport().event(this, "null");
     }
 }
