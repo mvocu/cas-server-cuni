859d70b82dbec38f913fe7bbcf5a76fc94c9ec36
Michal Vocu <michal@cas>
2026-04-17 09:20:13 +0200

    fixes from testing

--- etc/cas/scripts/notify-interrupt.groovy
+++ etc/cas/scripts/notify-interrupt.groovy
@@ -1,5 +1,8 @@
 import org.apereo.cas.interrupt.InterruptResponse
 
+import org.springframework.webflow.execution.RequestContextHolder
+import groovy.json.JsonOutput
+
 def run(final Object... args) {
     def principal = args[0]
     def attributes = args[1]
@@ -11,21 +14,27 @@ def run(final Object... args) {
     def block = false
     def ssoEnabled = true
 
-    def flowScope = requestContext?.getFlowScope()
+    def flowScope = requestContext.getFlowScope()
     def mfaAvailable = flowScope?.get("cuniMfaAvailableHandlers", [])
     def mfaEnabled = attributes?.cunimfapolicy?.contains("always") ?: false
 
-
     def response = new InterruptResponse(
-            "screen.interrupt.mfa.message(31.5.2026)",
-            [ "screen.interrupt.mfa.link": "https://ldapuser.cuni.cz/idportal/mfa"],
-            block,
-            ssoEnabled
+         "screen.interrupt.mfa.message_activate('31.5.2026', '31.10.2026')",
+         [ "activatemfa" : "https://ldapuser.cuni.cz/idportal/mfa"],
+         block,
+         ssoEnabled
     )
 
-    response.data = [
-            "mfaAvailable" : mfaAvailable.toString()
-    ]
+    /*
+    response.data = [ 
+       "mfaAvailable" : mfaAvailable.toString(),
+       "mfaPolicy" : attributes?.cunimfapolicy,
+       "mfaEnabled" : mfaEnabled,
+       "auth_loa" : attributes?.auth_loa,
+       "affiliation" : attributes?.edupersonscopedaffiliation
+    ] 
+    */
 
     return response
 }
+
